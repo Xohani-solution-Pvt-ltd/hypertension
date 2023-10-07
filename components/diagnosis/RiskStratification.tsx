3fc7737b3cc68a7da887076e405e03ac8f9eb3c9
pawanxohani <pawan.d@xohani.com>
2023-10-07 17:30:40 +0530

    create riskdata fetch api

--- components/diagnosis/RiskStratification.tsx
+++ components/diagnosis/RiskStratification.tsx
@@ -1,3 +1,223 @@
+// import { useEffect, useState } from 'react';
+
+// function MyComponent() {
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {
+//     fetch('/api/allFetchData') // Replace with your API endpoint
+//       .then((response) => response.json())
+//       .then((data) => setData(data))
+//       .catch((error) => console.error('Error fetching data:', error));
+//   }, []);
+
+//   return (
+//     <div>
+//       {data ? (
+//         <pre>{JSON.stringify(data, null, 2)}</pre>
+//       ) : (
+//         <p>Loading data...</p>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default MyComponent;
+
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const RiskStratification = () => {
+  const [results, setResults] = useState('');
+  const [criteria, setCriteria] = useState({
+    'cva': false,
+    'coronaryArteryDisease': false,
+    'previousHeartAttacks': false,
+    'breathlessness': false,
+    'cad': false,
+    'heartFailure': false,
+    'hfrEF': false,
+    'hfpeEF': false,
+    eGFR: 0,
+  });
+  const fetchCriteriaData = async () => {
+    console.log("out of try");
+    try {
+      const response = await fetch('http://localhost:3000/api/allFetchData');
+      const data = await response.json(); // Await the JSON parsing
+      console.log("data", data);
+      setCriteria({
+        'cva': data.comorbiditiesData.cva,
+        'coronaryArteryDisease': data.bloodTestData.coronaryArteryDisease === 'Present',
+        'previousHeartAttacks': data.symptomsData.previousHeartAttacks,
+        'breathlessness': data.symptomsData.breathlessness,
+        'cad': data.comorbiditiesData.coronaryArteryDisease === true,
+        'heartFailure': data.comorbiditiesData.heartFailure === true,
+        'hfrEF': data.bloodTestData.hfrEF > 35,
+        'hfpeEF': data.bloodTestData.hfpeEF > 35,
+        eGFR: data.bloodTestData.eGFR,
+      });
+    } catch (error) {
+      console.log("inside of catch");
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCriteriaData();
+  }, []); 
+
+  const checkRisk = () => {
+    let riskLevel = 'Low Risk';
+
+    if (criteria['cva']) {
+      riskLevel = 'High Risk (Probable CVA)';
+    } else if (
+      criteria['coronaryArteryDisease'] ||
+      criteria['previousHeartAttacks'] ||
+      criteria['breathlessness'] ||
+      criteria['cad']
+    ) {
+      riskLevel = 'High Risk (Probable CAD)';
+    } else if (
+      criteria['heartFailure'] ||
+      criteria['breathlessness'] ||
+      criteria['hfrEF'] ||
+      criteria['hfpeEF']
+    ) {
+      riskLevel = 'High Risk (Probable Heart failure)';
+    } else if (criteria.eGFR < 60) {
+      riskLevel = 'High Risk (CKD)';
+    }
+
+    setResults(riskLevel);
+  };
+
+  return (
+    <div>
+      <h1>High Risk Checker</h1>
+      {/* Implement input fields for criteria here */}
+      <button onClick={checkRisk}>Check Risk</button>
+      <p>{results}</p>
+    </div>
+  );
+};
+
+export default RiskStratification;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useEffect, useState } from "react";
+
+
+// const RiskStratification=()=>{
+// const [results, setResults] = useState('');
+// const [criteria, setCriteria] = useState({
+//   'cva': false,
+//   'coronaryArteryDisease': false,
+//   'previousHeartAttacks': false,
+//   'breathlessness': false,
+//   'cad': false,
+//   'heartFailure': false,
+//   'hfrEF': false,
+//   'hfpeEF': false,
+//   eGFR: 0,
+// });
+//   // Simulating data fetching from different databases
+//   const fetchCriteriaData = async () => {
+//     try {
+//       // Fetch data from different API endpoints for different databases
+//       const cvaDataResponse = await fetch('/api/diagnosis');
+//       const cvaData = await cvaDataResponse.json();
+//      console.log("cvaData",cvaData)
+//       const cadDataResponse = await fetch('/api/comorbidities');
+//       const cadData = await cadDataResponse.json();
+
+//       const heartFailureDataResponse = await fetch('/api/comorbidities');
+//       const heartFailureData = await heartFailureDataResponse.json();
+
+//       const eGFRDataResponse = await fetch('/api/blood');
+//       const eGFRData = await eGFRDataResponse.json();
+
+//       // Combine the fetched data from different sources
+//       const combinedData = {
+//         ...cvaData,
+//         ...cadData,
+//         ...heartFailureData,
+//         ...eGFRData,
+//       };
+
+//       // Update the criteria state based on the combined data
+//       setCriteria(combinedData);
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchCriteriaData();
+//   }, []); // Fetch data when the component mounts
+
+// const checkRisk = () => {
+//   // Implement your criteria checking logic here
+//   // Set the 'results' state accordingly
+
+//   // Example logic:
+//   let riskLevel = 'Low Risk';
+
+//   if (criteria['cva']) {
+//     riskLevel = 'High Risk (Probable CVA)';
+//   } else if (
+//     criteria['coronaryArteryDisease'] ||
+//     criteria['previousHeartAttacks'] ||
+//     criteria['breathlessness'] ||
+//     criteria['cad']
+//   ) {
+//     riskLevel = 'High Risk (Probable CAD)';
+//   } else if (
+//     criteria['heartFailure'] ||
+//     criteria['breathlessness'] ||
+//     criteria['hfrEF'] ||
+//     criteria['hfpeEF']
+//   ) {
+//     riskLevel = 'High Risk (Probable Heart failure)';
+//   } else if (criteria.eGFR < 60) {
+//     riskLevel = 'High Risk (CKD)';
+//   }
+
+//   setResults(riskLevel);
+// };
+
+// return (
+//   <div>
+//     <h1>High Risk Checker</h1>
+//     {/* Implement input fields for criteria here */}
+//     <button onClick={checkRisk}>Check Risk</button>
+//     <p>{results}</p>
+//   </div>
+// );
+// };
+
+// export default RiskStratification;
+
+
+
 // import React, { useState } from 'react'
 
 // const RiskStratification = () => {
@@ -8,40 +228,6 @@
 // }
 
 // export default RiskStratification
-
-
-
-// pages/[id].js
-import axios from 'axios';
-import { useRouter } from 'next/router';
-
-const UniqueIdComponent = ({ data }) => {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return (
-    <div>
-      <p>Unique ID: {id}</p>
-      {/* Display fetched data here... */}
-    </div>
-  );
-};
-
-export async function getServerSideProps({ params }) {
-  const { id } = params;
-
-  // Fetch data using 'id'
-  // Example using Axios:
-  // const response = await axios.get(`https://localhost:3000/api/auth/signup/${id}`);
-        
-  return {
-    props: {
-      // data: response.data,
-    },
-  };
-}
-
-export default UniqueIdComponent;
 
 
 // import { useEffect, useState } from "react";
@@ -156,95 +342,6 @@ export default UniqueIdComponent;
 
 
 
-
-//   const [results, setResults] = useState('');
-//   const [criteria, setCriteria] = useState({
-//     'cva': false,
-//     'coronaryArteryDisease': false,
-//     'previousHeartAttacks': false,
-//     'breathlessness': false,
-//     'cad': false,
-//     'heartFailure': false,
-//     'hfrEF': false,
-//     'hfpeEF': false,
-//     eGFR: 0,
-//   });
-//     // Simulating data fetching from different databases
-//     const fetchCriteriaData = async () => {
-//       try {
-//         // Fetch data from different API endpoints for different databases
-//         const cvaDataResponse = await fetch('/api/diagnosis');
-//         const cvaData = await cvaDataResponse.json();
-//        console.log("cvaData",cvaData)
-//         const cadDataResponse = await fetch('/api/comorbidities');
-//         const cadData = await cadDataResponse.json();
-
-//         const heartFailureDataResponse = await fetch('/api/comorbidities');
-//         const heartFailureData = await heartFailureDataResponse.json();
-
-//         const eGFRDataResponse = await fetch('/api/blood');
-//         const eGFRData = await eGFRDataResponse.json();
-
-//         // Combine the fetched data from different sources
-//         const combinedData = {
-//           ...cvaData,
-//           ...cadData,
-//           ...heartFailureData,
-//           ...eGFRData,
-//         };
-
-//         // Update the criteria state based on the combined data
-//         setCriteria(combinedData);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//       }
-//     };
-
-//     useEffect(() => {
-//       fetchCriteriaData();
-//     }, []); // Fetch data when the component mounts
-
-//   const checkRisk = () => {
-//     // Implement your criteria checking logic here
-//     // Set the 'results' state accordingly
-
-//     // Example logic:
-//     let riskLevel = 'Low Risk';
-
-//     if (criteria['cva']) {
-//       riskLevel = 'High Risk (Probable CVA)';
-//     } else if (
-//       criteria['coronaryArteryDisease'] ||
-//       criteria['previousHeartAttacks'] ||
-//       criteria['breathlessness'] ||
-//       criteria['cad']
-//     ) {
-//       riskLevel = 'High Risk (Probable CAD)';
-//     } else if (
-//       criteria['heartFailure'] ||
-//       criteria['breathlessness'] ||
-//       criteria['hfrEF'] ||
-//       criteria['hfpeEF']
-//     ) {
-//       riskLevel = 'High Risk (Probable Heart failure)';
-//     } else if (criteria.eGFR < 60) {
-//       riskLevel = 'High Risk (CKD)';
-//     }
-
-//     setResults(riskLevel);
-//   };
-
-//   return (
-//     <div>
-//       <h1>High Risk Checker</h1>
-//       {/* Implement input fields for criteria here */}
-//       <button onClick={checkRisk}>Check Risk</button>
-//       <p>{results}</p>
-//     </div>
-//   );
-// };
-
-// export default RiskStratification;
 
 // // Step 7: Risk Stratification Function
 // function calculateRisk(formData) {
---
+++ pages/api/allFetchData/index.ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectMongo } from "../../../utils/mongodb";
+import DiagnosisModel from "../../../models/diagnosis.model";
+import UserModel from "../../../models/user.model";
+import BloodTestModel from "../../../models/bloodtest.model";
+import ComorbiditiesModel from "../../../models/comorbidities.model";
+import SymptomModel from "../../../models/symptom.model";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    await connectMongo();
+
+    if (req.method === "GET") {
+      const id = req.query.id;
+      const [
+        diagnosisData,
+        comorbiditiesData,
+        symptomsData,
+        bloodTestData,
+      ] = await Promise.all([
+        DiagnosisModel.findOne({diagnosisId : id}),
+        ComorbiditiesModel.findOne({comorbiditiesId:id}),
+        SymptomModel.findOne({ symptomsId: id }),
+        BloodTestModel.findOne({bloodTestId : id})
+      ]);
+
+      res.status(200).json({
+        success: true,
+        message: "Data fetched successfully",
+        data: {
+          diagnosisData,
+          comorbiditiesData,
+          symptomsData,
+          bloodTestData
+        },
+      });
+    } else {
+      res.status(405).json({
+        success: false,
+        message: "Invalid method",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve Diagnosis",
+    });
+  }
+};
+
+export default handler;
